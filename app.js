455802e824f86a660b582fcd8d19e818c6050095
Saketkhare2000 <73240636+Saketkhare2000@users.noreply.github.com>
2021-01-27 19:49:11 +0530

    new changes

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 $(function () {
   $(document).scroll(function () {
+    console.log('Hello');
     var $nav = $('.navbar ');
+
     $nav.toggleClass('scrolled', $(this).scrollTop() > 10);
   });
 });
